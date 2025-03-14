2fbbaddf4fd9f8efa23e8a46ea335ba7b2247106
jeancharlesx64 <jean.albuquerque@sptech.school>
2025-03-14 17:15:00 -0300

    [FEAT] :: base structure of frontend using reactjs

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import AppRoutes from './routes/AppRoutes'
+
+export default function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </>
+  )
+}
+
+
